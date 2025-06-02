bd3f318e2791fa45a3ef009adf204f38b4234e4b
AsiaKabacinska <75363286+AsiaKabacinska@users.noreply.github.com>
2025-06-02 16:06:48 +0200

    configuration problems resolved

---
+++ dist/Drogeria.de5c0784.js
@@ -0,0 +1,41 @@
+// import "~normalize.css";
+const hamburger = document.querySelector(".hamburger--js");
+console.log("LALALA: " + hamburger);
+hamburger.addEventListener("click", ()=>{
+    const nav = document.querySelector(".navigation--js");
+    console.log("AAAA " + nav);
+    nav.classList.toggle("navigation--open");
+});
+const wyborItems = document.querySelectorAll("div.wybor__item");
+wyborItems.forEach((wyborItem)=>{
+    wyborItem.addEventListener("mouseenter", (e)=>{
+        console.log("ENTER");
+        let heading = e.target.querySelector(".wybor__item--heading");
+        heading.classList.toggle("wybor__item--heading--open");
+    });
+    wyborItem.addEventListener("mouseleave", (e)=>{
+        console.log("LEAVE");
+        let heading = e.target.querySelector(".wybor__item--heading");
+        heading.classList.toggle("wybor__item--heading--open");
+    });
+});
+const drogeria = document.querySelector(".heading__home");
+drogeria.addEventListener("click", ()=>{
+    window.location.pathname = "glownaStrona.html";
+});
+//koszyk
+const ceny = document.getElementsByClassName("produkty__item__btn");
+const koszyk = document.getElementById("koszyk");
+let ogolnaCena = 0;
+let item = 0;
+for(let i = 0; i < ceny.length; i++)ceny[i].addEventListener("click", dodajDoKoszyka);
+function dodajDoKoszyka(event) {
+    let cenaProduktu = parseFloat(event.target.value);
+    ogolnaCena += cenaProduktu;
+    item++;
+    //wpisz summary
+    const basketSummary = koszyk.querySelector(".basket__summary");
+    basketSummary.innerHTML = `Ilosc ${item}, warto\u{15B}\u{107}: ${ogolnaCena} PLN`;
+}
+
+//# sourceMappingURL=Drogeria.de5c0784.js.map
